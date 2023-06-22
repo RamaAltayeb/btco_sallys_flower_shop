11632723b2365b11aa1b379fd27e21ebef2ba9b1
RamaAltayeb_Coreline <rama.altayeb@coreline.tech>
2023-06-22 12:37:05 +0300

    [IMP] part1

---
+++ __manifest__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "BTCO Sally's Flower Shop",
+
+    'summary': """
+    """,
+
+    'description': """
+    """,
+
+    'author': 'Rama Altayeb',
+    'website': '',
+    'category': '',
+    'version': '16.0.0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': [],
+
+    # always loaded
+    'data': [
+        'security/ir.model.access.csv',
+        'views/flower.xml',
+    ],
+}
---
+++ models/flower.py
@@ -0,0 +1,13 @@
+from odoo import fields, models
+
+
+class Flower(models.Model):
+    _name = 'flower'
+    _description = 'Flower'
+
+    name = fields.Char(required=True)
+    scientific_name = fields.Char()
+    season_date_start = fields.Date()
+    season_date_end = fields.Date()
+    watering_frequency = fields.Integer(help='Should be watered once every x days')
+    watering_amount = fields.Float(help='In milliliters')
