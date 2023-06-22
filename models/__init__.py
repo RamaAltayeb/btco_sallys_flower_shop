c64230035d701c16bb579bae7be80cccc36ca2a9
RamaAltayeb_Coreline <rama.altayeb@coreline.tech>
2023-06-22 11:40:07 +0300

    [IMP] blank project

--- models/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-from . import flower
-from . import flower_water
-from . import product_product
-from . import product_template
-from . import stock_lot
-from . import stock_warehouse
-from . import stock_warehouse_weather
